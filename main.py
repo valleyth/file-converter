52c61508a28d6791ce56886822cd3501f2cbfa77
valley <ilyasvaleev56@gmail.com>
2026-08-01 23:49:24 +0300

    feat: тёмная тема через QSS

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from pathlib import Path
 import converters
 
 def main():
     app = QApplication(sys.argv)
+    style_path = Path(__file__).parent/"resources"/"styles"/"dark.qss"
+    if style_path.exists():
+        app.setStyleSheet(style_path.read_text(encoding="utf-8"))
+        
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
