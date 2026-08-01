823e2e398236604c4c045b24fd6da0f588f35b30
valley <ilyasvaleev56@gmail.com>
2026-08-02 01:13:10 +0300

    feat: иконка приложения

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from ui.main_window import MainWindow
 from pathlib import Path
 import converters
 
 def main():
     app = QApplication(sys.argv)
-    style_path = Path(__file__).parent/"resources"/"styles"/"dark.qss"
+    base_dir = Path(__file__).parent
+    style_path = base_dir / "resources" / "styles" / "dark.qss"
     if style_path.exists():
         app.setStyleSheet(style_path.read_text(encoding="utf-8"))
-        
+    icon_path = base_dir / "resources" / "icons" / "app_icon.png"
+    if icon_path.exists():
+        app.setWindowIcon(QIcon(str(icon_path)))    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
